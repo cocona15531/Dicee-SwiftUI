096931741aa8290f2a1b7b8948c9dca56f986962
issei.ueda <ron.ron633662@gmail.com>
2023-08-11 15:20:02 +0900

    first commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Issei Ueda on 2023/08/10.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
